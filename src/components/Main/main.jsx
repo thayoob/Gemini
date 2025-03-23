4896c933346a404f4e011da2875f0d895f40f05e
thayoob <muyyuddeenthayoob@gmail.com>
2025-03-23 18:08:46 +0530

    desgin response as best way

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -45,7 +45,14 @@ const main = () => {
                         </div>
                         <div className="result-data">
                             <img src={assets.gemini_icon} alt="" />
-                            <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            {loading
+                                ? <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            }
                         </div>
                     </div>
                 }
