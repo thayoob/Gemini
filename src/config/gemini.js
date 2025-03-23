28957184182b0655c14e9c9a111cf11c4fcf2c89
thayoob <muyyuddeenthayoob@gmail.com>
2025-03-23 17:11:35 +0530

    immplemt the gemini API

---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+const GEMINI_API_KEY = ""
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+});
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+};
+
+async function run(promt) {
+    const chatSession = model.startChat({
+        generationConfig,
+        history: [
+        ],
+    });
+
+    const result = await chatSession.sendMessage(promt);
+    console.log(result.response.text());
+
+    return result.response.text();
+}
+
+export default run;
